bd6c5a6c839d9d82de86c414b56c90e68bd66ae7
Paulo Henrique Ferreira Lopes <paulohenrique1502@hotmail.com>
2023-02-02 18:43:40 -0300

    conseguii o que eu queria

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
-function acessar(){
-    let cidade = String(document.querySelector(`ìnput#itext`)).value
-    let res = document.querySelector(`h1#res`)
-    res.innerHTML = `Tempo em ${cidade}`
-    //if(cidade == 0){
-        //window.alert('Digite Algo Para Continuar')
-    //}else{
-        //res.innerHTML = `Tempo em ${cidade}`
-    //}
+function Verificar(){
+    var cidade = String(document.querySelector(`input#itext`).value)
+    var res = document.querySelector(`h1#res`)
+    res.innerHTML +=''
     
+    if(cidade == 0){
+        window.alert('Digite Alguna Cidade Para Continuar')
+    }else{
+        res.innerHTML = `Tempo em ${cidade}`
+    }
 }
+
+    
